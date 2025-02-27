dd01ba6b842d032d6d80e86f773df01699a59392
ald-diogo <adaomanuek@gmail.com>
2025-02-27 21:17:22 +0000

    Tarefa 01 em progresso.

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0ad4223fec7139c6e5584f9ccca89a2c0e282c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+722ea7b5017b4d71c1398d2aa84acd588e8e1286")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
