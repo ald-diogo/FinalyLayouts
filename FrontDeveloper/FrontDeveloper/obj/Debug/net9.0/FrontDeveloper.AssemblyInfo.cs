b9a8830780b322411d571f4da8d64df4a53c5380
ald-diogo <adaomanuek@gmail.com>
2025-02-28 12:47:26 +0000

    Add Snackbar no botão Ver Ofertas.

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca7fac27dbd9412fd0e499b61fdd0cfc66afb421")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc625ed754811279f568d86abfbf8abe84a555c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
