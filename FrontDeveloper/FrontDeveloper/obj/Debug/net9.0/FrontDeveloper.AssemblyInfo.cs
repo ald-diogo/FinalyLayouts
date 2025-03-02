a96ab66fe4b0abe8a095cc1d15d68b758cea8b6b
ald-diogo <adaomanuek@gmail.com>
2025-03-02 19:37:26 +0000

    Gestão
    Add data

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c986931007a0460caf2b4882475cbd9f17a612d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a714380fd5528a837c2bc8161fe95406ba5c176")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
