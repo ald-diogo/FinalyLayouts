5c6e2c65dbf6781246800caff726291d5e1e247f
ald-diogo <adaomanuek@gmail.com>
2025-03-02 19:54:14 +0000

    Gestão
    Em progresso Crud

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a714380fd5528a837c2bc8161fe95406ba5c176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a96ab66fe4b0abe8a095cc1d15d68b758cea8b6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
