cba82b1ae8bf028e7d90aa408b3a0ad26b09781e
ald-diogo <adaomanuek@gmail.com>
2025-03-03 07:54:57 +0000

    Gestão
    Em curso cadastro.

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c6e2c65dbf6781246800caff726291d5e1e247f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c986a7b739f24b0f5f7d114cbae604d1e23683be")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
