37c7a5e43b24e39da2625532f5b621ec7ddf57e7
ald-diogo <adaomanuek@gmail.com>
2025-03-05 10:02:36 +0000

    Gestao
    Feat: Add icones nas tabs

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24a9e0866de56a143a756cecbf44a8a7d5961b9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd39e87abc6398e89768125f0b548bafef64cb44")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
