0a0ebae95af3fe4da63aeb6c31b9c9cbe1afab62
ald-diogo <adaomanuek@gmail.com>
2025-03-06 21:07:21 +0000

    Gestao
    Feat:EstudanteService

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5cd980659f521e422e54d84a5d958cb47b62bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a79caa0f8e96e8caeb349587ea73de72bbe42c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
