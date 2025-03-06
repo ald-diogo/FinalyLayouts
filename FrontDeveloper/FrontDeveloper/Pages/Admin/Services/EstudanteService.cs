606d05e1f0e9050d105fc02cc0e9295846f47395
ald-diogo <adaomanuek@gmail.com>
2025-03-06 21:38:44 +0000

    Gestao
    Feat: EstudanteService.cs

--- FrontDeveloper/FrontDeveloper/Pages/Admin/Services/EstudanteService.cs
+++ FrontDeveloper/FrontDeveloper/Pages/Admin/Services/EstudanteService.cs
@@ -1,6 +1,15 @@
-﻿namespace FrontDeveloper.Pages.Admin;
+﻿using System.Collections.Generic;
+using FrontDeveloper.Models;
+using FrontDeveloper.Pages.Admin;
+
+namespace FrontDeveloper.Pages.Admin;
 
 public class EstudanteService
 {
-    
+    public List<Estudante> Estudantes { get; private set; } = new List<Estudante>();
+
+    public void AdicionarEstudante(Estudante estudante)
+    {
+        Estudantes.Add(estudante);
+    }
 }
