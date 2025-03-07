ce126842ee4f4f24d7ea4f00fba5096efc084a3c
ald-diogo <adaomanuek@gmail.com>
2025-03-07 22:11:23 +0000

    Gestao
    em curso manual

--- BlazorAppReserva/BlazorAppReserva/obj/Debug/net9.0/BlazorAppReserva.AssemblyInfo.cs
+++ BlazorAppReserva/BlazorAppReserva/obj/Debug/net9.0/BlazorAppReserva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppReserva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+704365f04cd7ce28121516c7354694498142fd79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+664be41cbaf3ae74fc32476176366033f7febf0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppReserva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppReserva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
