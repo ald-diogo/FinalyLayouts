3dbe6a47206ae9d65ed268aea3b3f57b27d39ea9
ald-diogo <adaomanuek@gmail.com>
2025-03-09 22:38:49 +0000

    Gestao
    Add Proprietario

--- BlazorAppReserva/BlazorAppReserva/obj/Debug/net9.0/BlazorAppReserva.AssemblyInfo.cs
+++ BlazorAppReserva/BlazorAppReserva/obj/Debug/net9.0/BlazorAppReserva.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorAppReserva")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9de038f5af93835e3238e9302126b67c9c52ea14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d225a3d4e8475010e63cf57f86f2bfc8dc8cd3bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorAppReserva")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorAppReserva")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
