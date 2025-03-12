a614f7e54da066356994478cf0f5f7538968ad0d
ald-diogo <adaomanuek@gmail.com>
2025-03-12 09:49:16 +0000

    Gestao
    Feat: Add icons

--- FrontDeveloper/FrontDeveloper/Pages/Services/ProfessorService.cs
+++ FrontDeveloper/FrontDeveloper/Pages/Services/ProfessorService.cs
@@ -1,6 +1,65 @@
-﻿namespace FrontDeveloper.Pages.Services;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FrontDeveloper.Pages.Models;
+
+namespace FrontDeveloper.Pages.Services;
 
 public class ProfessorService
 {
+    // Lista que armazena os professores cadastrados
+    private List<Professor> professores = new();
+
+    // Retorna a lista de professores
+    public List<Professor> GetProfessores() => professores;
+
+    // Adiciona um novo professor à lista
+    public void AdicionarProfessor(Professor professor)
+    {
+        professor.ProfessorId = professores.Count + 1; // Define um ID único para o professor
+        professores.Add(professor); // Adiciona o professor à lista
+    }
+
+    // Edita um professor existente na lista
+    public void EditarProfessor(Professor professor)
+    {
+        // Busca o professor pelo ID
+        var existente = professores.Find(p => p.ProfessorId == professor.ProfessorId);
+        if (existente != null)
+        {
+            // Atualiza os dados do professor
+            existente.NomeCompleto = professor.NomeCompleto;
+            existente.Genero = professor.Genero;
+            existente.DataAdmissao = professor.DataAdmissao;
+            existente.NumeroIdentificacao = professor.NumeroIdentificacao;
+            existente.Departamento = professor.Departamento;
+            existente.DisciplinasLecionadas = professor.DisciplinasLecionadas;
+            existente.Nacionalidade = professor.Nacionalidade;
+            existente.EstadoCivil = professor.EstadoCivil;
+            existente.GrauAcademico = professor.GrauAcademico;
+            existente.NumeroFuncionario = professor.NumeroFuncionario;
+            existente.RegimeTrabalho = professor.RegimeTrabalho;
+            existente.ExperienciaAnos = professor.ExperienciaAnos;
+            existente.NomeUsuario = professor.NomeUsuario;
+            existente.Senha = professor.Senha;
+            existente.Ativo = professor.Ativo;
+            existente.Telefone = professor.Telefone;
+            existente.Endereco = professor.Endereco;
+            existente.Email = professor.Email;
+        }
+    }
+
+    // Remove um professor da lista com base no ID
+    public void RemoverProfessor(int id)
+    {
+        professores.RemoveAll(p => p.ProfessorId == id); // Remove o professor pelo ID
+    }
     
+    // Método para buscar professores pelo nome (ignora maiúsculas e minúsculas)
+    public List<Professor> BuscarProfessorPorNome(string nome)
+    {
+        // Retorna todos os professores cujo nome contenha o termo informado (case insensitive)
+        return professores
+            .Where(p => p.NomeCompleto.Contains(nome, System.StringComparison.OrdinalIgnoreCase))
+            .ToList();
+    }
 }
--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ab6f183af19ad7583eff72d7c2d35d609a70c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2721cb3985f331aac6c134f7dd5559da9d92224")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
