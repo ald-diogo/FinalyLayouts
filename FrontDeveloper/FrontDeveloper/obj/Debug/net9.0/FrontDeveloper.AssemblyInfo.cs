6686c496a0e563695b9be31fc5bd3efc92c3d730
ald-diogo <adaomanuek@gmail.com>
2025-03-16 16:38:33 +0000

    Gestao
    Feat: Periodo

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4f2e9355835063021634f9e20fe5997c451ec0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab258ba4aacffe0c923dcddd7d4c1f4fd9501c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
