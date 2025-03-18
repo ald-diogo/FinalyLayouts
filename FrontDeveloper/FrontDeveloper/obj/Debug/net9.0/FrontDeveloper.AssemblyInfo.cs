c72a4501be89f8c69a6b6dc06dfdd3260ddfc098
ald-diogo <adaomanuek@gmail.com>
2025-03-18 23:55:52 +0000

    Gestao
    Feat 👍 Matricula

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278037a14c5bfc07fe9359567572bddb388596b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71678c4323801be9bd8517cc96bc18be3a20171b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
