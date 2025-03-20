d3c9762f403213db50be37696cdc5e40781b382c
ald-diogo <adaomanuek@gmail.com>
2025-03-20 09:21:06 +0100

    Gestao
    Feat 👍 Alternancia de tema por intermedio do sistema operacional.

--- FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
+++ FrontDeveloper/FrontDeveloper/obj/Debug/net9.0/FrontDeveloper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b1261e8b1b7fa734178101f23b18f555cfeccd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b7fac40344b0cb7b7f9c3451eafdc33784b1d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrontDeveloper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ FrontDeveloper/FrontDeveloper/wwwroot/theme.js
@@ -0,0 +1,17 @@
+﻿// Função que retorna se o sistema está no modo escuro
+window.getSystemTheme = () => {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+};
+
+// Função que observa mudanças no tema do sistema e comunica o Blazor
+window.watchSystemTheme = (dotnetHelper) => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Adiciona um ouvinte para detectar mudanças no tema
+    const listener = (event) => {
+        dotnetHelper.invokeMethodAsync("SetDarkMode", event.matches);
+    };
+
+    mediaQuery.addEventListener("change", listener);
+};
+
